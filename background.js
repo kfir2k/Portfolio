504987e6e130742bb076cd1bb1bd55c19439d578
Kfir Kotler <kfir.kotler@gmail.com>
2024-01-01 18:33:34 +0200

    My Portfilio

---
+++ background.js
@@ -0,0 +1,16 @@
+VANTA.NET({
+	el: "#hero-image",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 1.00,
+	scaleMobile: 1.00,
+	color: 0xffffff,
+	backgroundColor: 0xc5c5c5,
+	points: 17.00,
+	maxDistance: 25.00,
+	spacing: 17.00,
+	showDots: false
+})
