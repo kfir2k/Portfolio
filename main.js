e02fb5d03b95949256a0a6b88bbe3f6b8012f276
Kfir Kotler <kfir.kotler@gmail.com>
2024-01-02 01:48:48 +0200

    1

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // ===================Website ui ================
 
 console.log("test");
-window.addEventListener("scroll", animationSrtart)
-function animationSrtart() {
+window.addEventListener("scroll", animationStart)
 
+function animationStart() {
+    
     console.log(window.scrollY);
-
 
     switch (true) {
         case window.scrollY === 0:
@@ -14,11 +14,46 @@ function animationSrtart() {
         case window.scrollY > 90 && window.scrollY < 800:
             document.getElementById("header").style.background = 'rgba(250,250,250,0.8)';
             break;
+    }
+}
 
 
+window.onscroll = function () {
+    
+    if (document.documentElement.scrollTop > 500) {
+        progressBar("html", 100)
     }
 
+};
 
 
 
+
+
+
+
+
+function progressBar(el,precent) {
+    el = document.getElementById(el);
+    let start = 0
+    let interval = setInterval(() => {
+        if (start >= precent) {
+            clearInterval(interval)
+            console.log("interval cleard");
+        } else {
+            start++
+            console.log(start);
+            el.innerHTML = `${start}%`
+            el.style.width = `${start}%`
+        }
+        console.log("interval cleard done!!!!!!!!!!!");
+    
+    }, 10)
+
+
+    
 }
+
+
+//progressBar()
+
