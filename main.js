2a076e90898c4fb3f20f1bb857541c100e131f0d
kfir <kfir.kotler@gmail.com>
2024-01-01 22:20:02 +0200

    fixed header and added scroll background for it with js

---
+++ main.js
@@ -0,0 +1,24 @@
+// ===================Website ui ================
+
+console.log("test");
+window.addEventListener("scroll", animationSrtart)
+function animationSrtart() {
+
+    console.log(window.scrollY);
+
+
+    switch (true) {
+        case window.scrollY === 0:
+            document.getElementById("header").style.background = 'rgba(0,0,0,0.0)';
+            break;
+        case window.scrollY > 100 && window.scrollY < 800:
+            document.getElementById("header").style.background = 'rgba(250,250,250,0.8)';
+            break;
+
+
+    }
+
+
+
+
+}
